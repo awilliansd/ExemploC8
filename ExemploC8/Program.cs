d8e9b67920e88579e2613232dc13105e66062eaa
Alessandro Dias <awilliansd@protonmail.com>
2019-02-20 21:48:51 -0400

    Novas implementações

--- ExemploC8/Program.cs
+++ ExemploC8/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace ExemploC8
 {
@@ -51,6 +53,101 @@ namespace ExemploC8
             }
 
             Console.Write("  ]" + Environment.NewLine);
+        }
+
+        static string GetString()
+        {
+            string test = "This is a test";
+            test = null;
+            return test;
+        }
+
+        static string GetNullableString()
+        {
+            string? test = "This is a test";
+            test = null;
+            return test;
+        }
+
+        static async IAsyncEnumerable<Person> GetAsync(IAsyncEnumerable<Person> people)
+        {
+            await foreach (var t in people)
+            {
+                yield return t;
+            }
+        }
+
+        // Agora não há necessidade de ficar usando Take e Skip para selecionar uma porção específica do array
+        static IEnumerable<Person> SliceArray(Person[] people)
+        {
+            List<Person> newList = new List<Person>();
+
+            foreach (var person in people[1..5])
+            {
+                newList.Add(person);
+            }
+
+            foreach (var person in people[1..^2])
+            {
+                newList.Add(person);
+            }
+
+            return newList;
+        }
+
+        static string GetRecursivePatterns(Person person)
+        {
+            switch (person.Name, person.LastName)
+            {
+                case (string n, string s):
+                    return $"{n} {s}";
+                case (string n, null):
+                    return n;
+                case (null, string s):
+                    return $"Sr. {s}";
+                case (null, null):
+                    return "Desconhecido";
+            }
+        }
+
+        static string GetSwitchExpressions(Person person)
+        {
+            return (person.Name, person.LastName) switch
+            {
+                (string n, string s) => $"{n} {s}",
+                (string n, null) => n,
+                (null, string s) => $"Sr. {s}",
+                (null, null) => "Desconhecido"
+            };
+        }
+
+        //private Dictionary<string, string> dictionary = new ();
+
+        static void GetImplicityNewExpressions()
+        {
+            //Point[] ps = { new (1, 4), new (3,-2), new (9, 5) };
+            int[] data5 = { 1, 2, 3 };
+            var a = new Func<int, bool>(a => a == 1);
+            
+
+        //Person[] pessoas = new
+        //    {
+        //        new ("Henrique", "Dal Bello"),
+        //        new ("Bill", "Gates"),
+        //        new ("Mark", "Zuckerberg")
+        //    }
+        }
+    }
+
+    internal class Person
+    {
+        public string Name { get; internal set; }
+        public string LastName { get; internal set; }
+
+        public Person(string name, string lastName)
+        {
+            Name = name;
+            LastName = lastName;
         }
     }
 }
